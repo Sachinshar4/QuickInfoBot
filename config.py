f7a90cca9a4b53b1889d4a1333a1b918fea519df
Sachinshar4 <130998113+Sachinshar4@users.noreply.github.com>
2025-11-20 22:25:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
+# Channel: https://t.me/Info_digger1_bot
 API_ID = "39510920"  
 API_HASH = "a340ca518730a0d245c1e2174a63e162"  
 BOT_TOKEN = "8583803720:AAElqgulWfgNJEKyiFo1xptj1gwb2zMgTHQ"  
 COMMAND_PREFIX = "#|.|/|!|,"
 
 ADMIN_ID = 6436766847
+
